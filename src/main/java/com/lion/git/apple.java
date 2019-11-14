2d2868b50f40b0874c3371e7061715470c5660d9
eclipse_user <eclipse_user@163.com>
2019-11-14 16:39:12 +0800

    My first commit in Eclipse

---
+++ src/main/java/com/lion/git/apple.java
@@ -0,0 +1,9 @@
+package com.lion.git;
+
+public class apple {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+	}
+
+}
---
+++ src/test/java/com/lion/test/GoodTest.java
@@ -0,0 +1,10 @@
+package com.lion.test;
+
+public class GoodTest {
+
+	public static void main(String[] args) {
+		System.out.println("2");
+
+	}
+
+}
