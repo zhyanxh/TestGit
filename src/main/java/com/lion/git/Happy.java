afe8a9a4f7c92ebbac21663e425f6b747474edf9
lion_glb <814500577@qq.com>
2019-11-15 08:51:31 +0800

    test conflict right

--- src/main/java/com/lion/git/Happy.java
+++ src/main/java/com/lion/git/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("test ...");
 		System.out.println("more test ...");
+		System.out.println("test conflict right");
 	}
 }
